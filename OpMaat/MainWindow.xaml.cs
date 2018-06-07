718a5c441b8b78eea9525a130e35ed80369d1fc4
Aster Roelandt <aster.roelandt@avo.la>
2018-06-07 11:15:30 +0200

    Add project files.

---
+++ OpMaat/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Microsoft.Win32;
+
+namespace OpMaat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public VideoPlayer playerWindow;
+        public List<Uri> videoList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            playerWindow = new VideoPlayer();
+            videoList = new List<Uri>();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            listBox.Items.Add("Nog geen bestand(en) gekozen");
+        }
+
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void uploadButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Multiselect = true,
+                DefaultExt = ".mp4",
+                Filter = "MP4 files (*.mp4)|*.mp4",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            };
+            if (openFileDialog.ShowDialog() != true) return;
+
+            foreach (var fileName in openFileDialog.FileNames)
+            {
+                if(videoList.Count == 0)
+                    listBox.Items.Clear();
+                videoList.Add(new Uri(fileName));
+                listBox.Items.Add(fileName);
+            }
+
+            BestandLabel.Visibility = Visibility.Visible;
+            StartButton.IsEnabled = true;
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            playerWindow.VideoList = videoList;
+            playerWindow.mediaPlayer.Source = videoList[0];
+            playerWindow.Show();
+            var seconden = 10;
+            try
+            {
+                seconden = int.Parse(PauzeTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                // ignored
+            }
+            playerWindow.PauzeTimer.Interval = seconden * 1000;
+            playerWindow.PauzeTimer.Start();
+            playerWindow.mediaPlayer.Play();
+
+            this.Close();
+        }
+
+        private new void PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private static bool IsTextAllowed(string text)
+        {
+            var regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
+            return !regex.IsMatch(text);
+        }
+    }
+}
---
+++ OpMaat/VideoPlayer.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+using System.Windows;
+using System.Windows.Input;
+
+namespace OpMaat
+{
+    /// <summary>
+    /// Interaction logic for VideoPlayer.xaml
+    /// </summary>
+    public partial class VideoPlayer : Window
+    {
+        public int CurrentItem = 1;
+        public readonly Timer PauzeTimer = new Timer();
+        public List<Uri> VideoList;
+
+        public VideoPlayer()
+        {
+            KeyDown += mediaPlayer_KeyDown;
+            InitializeComponent();
+            PauzeTimer.Elapsed += pauzeTimer_Elapsed;
+            mediaPlayer.MediaEnded += mediaPlayer_ended;
+        }
+
+        private void MediaPlayer_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (!PauzeTimer.Enabled)
+            {
+                mediaPlayer.Play();
+                PauzeTimer.Start();
+            }
+        }
+
+        private void mediaPlayer_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                MainWindow window = new MainWindow();
+                mediaPlayer.Pause();
+                mediaPlayer.Source = null;
+                window.Show();
+                Close();
+            }
+        }
+
+        private void pauzeTimer_Elapsed(object source, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                mediaPlayer.Pause();
+            });
+            PauzeTimer.Stop();
+        }
+
+        private void mediaPlayer_ended(object source, RoutedEventArgs e)
+        {
+            if (CurrentItem < VideoList.Count)
+            {
+                mediaPlayer.Close();
+                mediaPlayer.Source = VideoList[CurrentItem];
+                mediaPlayer.Play();
+
+                CurrentItem++;
+            }
+            else
+            {
+                var window = new MainWindow();
+
+                mediaPlayer.Source = null;
+                window.Show();
+                Close();
+            }
+        }
+
+    }
+}
